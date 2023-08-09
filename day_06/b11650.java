4ae58265e904cd9625f9df3c8c39c6c4b51695f1
YouJin-Roh <89447924+YouJinRoh-SSU@users.noreply.github.com>
2023-08-09 13:13:05 +0900

    Add files via upload

---
+++ day_06/b11650.java
@@ -0,0 +1,12 @@
+package day_06;
+
+import java.util.Scanner;
+
+public class b11650 {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int m = kb.nextInt();
+		int n = kb.nextInt();
+	}
+
+}
